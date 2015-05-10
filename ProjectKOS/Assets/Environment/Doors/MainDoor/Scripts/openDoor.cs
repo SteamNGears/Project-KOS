48bf6acec0376eaf572dc7b4bee239909933c9f3
SteamNGears <jwshortcircuit@gmail.com>
2015-05-09 23:36:28 -0700

    Added simple glass doors with animation.

---
+++ ProjectKOS/Assets/Environment/Doors/MainDoor/Scripts/openDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openDoor : MonoBehaviour {
+
+	Animator anim;
+	//public something[] stuff;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		anim.StopPlayback();
+		anim.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player") {
+			anim.enabled = true;
+			anim.Play ("OpenDoor");
+		}
+	}
+
+}
