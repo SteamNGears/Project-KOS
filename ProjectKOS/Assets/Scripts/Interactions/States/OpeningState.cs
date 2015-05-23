8f1e4cc027358ebd80f948345f37e6f5c4e6d03b
SteamNGears <jwshortcircuit@gmail.com>
2015-05-22 23:30:13 -0700

    Added locking state.

--- ProjectKOS/Assets/Scripts/Interactions/States/OpeningState.cs
+++ ProjectKOS/Assets/Scripts/Interactions/States/OpeningState.cs
@@ -7,34 +7,48 @@
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+
+
+/**
+ * Filename: OpeningState
+ * Author: Jakob Wilson
+ * Created: 5/22/2015
+ * Revision: 1
+ * Rev. Date: 
+ * Rev. Author: 
+ * */
 using System;
 using UnityEngine;
 namespace States
 {
+	/**
+	 * A momentary state that simply plays the door opening animation, 
+	 * changes the emission color of the main material to green, 
+	 * then returns an idle state so no more actions can be done to the door.
+	 * */
 	public class OpeningState:InteractionState
 	{
 		private Animator anim;/**The animator so we cna playback animations*/
 		private Renderer rend;/**The renderer so we can change the color of the frame lights*/
 
-		public OpeningState (GameObject _actee, GameObject _actor):base(_actee, _actor)
-		{
-			this.anim = this.actee.GetComponent<Animator> ();
-			this.rend = this.actee.GetComponentInChildren<Renderer>();
-
-			// if we have an animator, play the animation
-			if(this.anim != null)
-				this.anim.Play("OpenDoors");
-
-			//if we have the renderer, change the frame light color
-			if(this.rend != null)
-			{
-				rend.material.EnableKeyword ("_EMISSION");
-				rend.material.SetColor("_EmissionColor", Color.green);
-			}
-		}
+		public OpeningState (GameObject _actee, GameObject _actor):base(_actee, _actor){}
 
 		public override InteractionState Behave ()
 		{
+				this.anim = this.actee.GetComponent<Animator> ();
+				this.rend = this.actee.GetComponentInChildren<Renderer>();
+				
+				// if we have an animator, play the animation
+				if(this.anim != null)
+					this.anim.Play("OpenDoors");
+				
+				//if we have the renderer, change the frame light color
+				if(this.rend != null)
+				{
+					rend.material.EnableKeyword ("_EMISSION");
+					rend.material.SetColor("_EmissionColor", Color.green);
+				}
+				
 				return new IdleState(this.actee, this.actor);
 		}
 	}
