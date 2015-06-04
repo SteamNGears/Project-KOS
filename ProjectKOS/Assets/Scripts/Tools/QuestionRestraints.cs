c923230cb3805f9b74d6dfbb809a1e1d966debc3
SteamNGears <jwshortcircuit@gmail.com>
2015-06-04 08:22:38 -0700

    Fixed upper/lower method name issue

--- ProjectKOS/Assets/Scripts/Tools/QuestionRestraints.cs
+++ ProjectKOS/Assets/Scripts/Tools/QuestionRestraints.cs
@@ -32,18 +32,18 @@ public class QuestionRestraints : MonoBehaviour
 		get
 		{
 			QuestionQuery query = new QuestionQuery ();										//create a query
-			query.addRestraint(new DifficultyRestraint(Min_Difficulty, Max_Difficulty));	//add the difficulty restraint
+			query.AddRestraint(new DifficultyRestraint(Min_Difficulty, Max_Difficulty));	//add the difficulty restraint
 
 			if(Question_Type == type.MULTIPLE_CHOICE)										//add the correct restrainr based o the Question_Type
-					query.addRestraint (new TypeRestraint ("MULTIPLE_CHOICE"));				// selected by the user
+					query.AddRestraint (new TypeRestraint ("MULTIPLE_CHOICE"));				// selected by the user
 			if(Question_Type == type.SHORT_ANSWER)
-					query.addRestraint (new TypeRestraint ("SHORT_ANSWER"));
+					query.AddRestraint (new TypeRestraint ("SHORT_ANSWER"));
 			if(Question_Type == type.TRUE_FALSE)
-					query.addRestraint (new TypeRestraint ("TRUE_FALSE"));
+					query.AddRestraint (new TypeRestraint ("TRUE_FALSE"));
 
 			foreach(string s in subjects)													//add any subject restraints that the user wanted
 			{
-				query.addRestraint (new SubjectRestraint (s));
+				query.AddRestraint (new SubjectRestraint (s));
 			}
 			return query;
 		}
