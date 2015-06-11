f32e34fe805e2a69d04e83b374111d6e400489a2
ArykLAnderson <aryk.anderson@gmail.com>
2015-06-11 09:39:09 -0700

    Saving and Loading works

--- ProjectKOS/Assets/Scripts/SaveLoad/SaveTestScript.cs
+++ ProjectKOS/Assets/Scripts/SaveLoad/SaveTestScript.cs
@@ -6,16 +6,16 @@ using SaveLoad;
 public class SaveTestScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-
+	void Start () 
+	{
         QuestionPool testPool = DatabaseConnector.Instance.GetQuestions(new QuestionQuery());
 
         SaveLoadManager.Instance.SaveObject += testPool.SaveObject;
         SaveLoadManager.Instance.LoadObject += testPool.LoadObject;
 
-        //string savePath = SaveLoadManager.Instance.SaveGame();
-        //Debug.Log(savePath);
+        string savePath = SaveLoadManager.Instance.SaveGame();
+        Debug.Log(savePath);
 
-		SaveLoadManager.Instance.LoadGame(SaveLoadManager.SaveFileDirectory + "63569602935146.save");
+		SaveLoadManager.Instance.LoadGame(savePath);
 	}
 }
